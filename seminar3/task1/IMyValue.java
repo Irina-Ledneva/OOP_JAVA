9250defe34f4e47f70d64d558e755d4d9f999d20
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-08-11 17:52:16 +0300

    seminar3

---
+++ seminar3/task1/IMyValue.java
@@ -0,0 +1,6 @@
+package seminar3.task1;
+
+public interface IMyValue {
+    double getValue(double v, double t);
+
+}
---
+++ seminar3/task1/IMyValue2.java
@@ -0,0 +1,7 @@
+package seminar3.task1;
+
+public interface IMyValue2 {
+
+    double getValue(double v);
+
+}
