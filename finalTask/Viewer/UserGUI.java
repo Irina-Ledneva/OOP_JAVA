d3b263e40f6769cdcd6164dd3454e7551b96327a
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-08-18 14:24:40 +0300

    finaltask

---
+++ finalTask/Viewer/UserGUI.java
@@ -0,0 +1,26 @@
+package finalTask.Viewer;
+
+/**
+ * @apiNote Interface for the user to interact with the GUI
+ */
+public interface UserGUI {
+
+    Double inputFirstValue();
+
+    Double inputSecondValue();
+
+    void simpleCalculatorMenu();
+
+    void complexCalculatorMenu();
+
+    void selectionButton();
+
+    Double inputRealPartFirstValue();
+
+    Double inputImaginaryPartFirstValue();
+
+    Double inputRealPartSecondValue();
+
+    Double inputImaginaryPartSecondValue();
+
+}
